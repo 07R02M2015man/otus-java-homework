44d640a296a2f39de2d1a5911bde943c5f8487aa
TatyanaM <nusha_kup@mail.ru>
2024-05-26 18:32:32 +0300

    Edit homework 9

--- src/main/java/ru/otus/java/basic/list/mainApp.java
+++ src/main/java/ru/otus/java/basic/list/mainApp.java
@@ -2,98 +2,89 @@ package ru.otus.java.basic.list;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class mainApp {
     public static void main(String[] args) {
-        ArrayList<Integer> list1 = new ArrayList<>(Arrays.asList(5, 7, 2, 3, 6));
-        arrayFromTo(4, 10);
-        System.out.println("sumElements(list1) = " + sumElements(list1));
-        increaseList(2, list1);
-        rewriteList(5, list1);
+        List<Integer> list1 = new ArrayList<>(Arrays.asList(5, 7, 2, 3, 6));
+        System.out.println("Список значений в интервале от 4 до 10: " + getListFromTo(4, 10));
+        System.out.println("Сумма элементов списка, значения которых больше больше 5: " + sumElements(list1));
+        System.out.println("Новый список, каждый элемент которого увеличен на 2: " + increaseList(2, list1));
+        System.out.println("Перепианный список: " + rewriteList(5, list1));
 
-        ArrayList<Worker> listWorkers = new ArrayList<>();
+        System.out.println();
+        List<Worker> listWorkers = new ArrayList<>();
         listWorkers.add(new Worker("name1", 30));
         listWorkers.add(new Worker("name2", 20));
         listWorkers.add(new Worker("name3", 22));
-        listWorkers.add(new Worker("name4", 20));
+        listWorkers.add(new Worker("name4", 40));
         System.out.println("Исходный список сотрудников: " + listWorkers);
 
-        listNameWorkers(listWorkers);
-        minAgeWorkers(listWorkers, 22);
-        System.out.println("Превышает средний возраст сотрудников заданное минимальное значение: " + averageAge(listWorkers, 25));
+        System.out.println("Список имен сотрудников: " + getNamesWorkers(listWorkers));
+        System.out.println("Список сотрудников старше 22 лет: " + getMinAgeWorkers(listWorkers, 22));
+        System.out.println("Средний возраст сотрудников превышает 25: " + checkAverageAge(listWorkers, 25));
         System.out.println("Самый молодой сотрудник: " + yangestWorker(listWorkers));
 
     }
 
-    //метод, возвращающий  самого молодого сотрудника
-    private static String yangestWorker(ArrayList<Worker> listWorkers) {
+    private static Worker yangestWorker(List<Worker> listWorkers) {
         int minAge = listWorkers.getFirst().getAge();
         Worker yangestWorker = listWorkers.getFirst();
-        for (int i = 1; i < listWorkers.size(); i++) {
-            if (listWorkers.get(i).getAge() < minAge) {
-                minAge = listWorkers.get(i).getAge();
-                yangestWorker = listWorkers.get(i);
+        for (Worker wrk : listWorkers) {
+            if (wrk.getAge() < minAge) {
+                minAge = wrk.getAge();
+                yangestWorker = wrk;
             }
         }
-        return yangestWorker.toString();
+        return yangestWorker;
     }
 
-    // метод, проверяющий что средний возраст сотрудников превышает указанный аргумент
-    private static boolean averageAge(ArrayList<Worker> listWorkers, int minAverAge) {
+    private static boolean checkAverageAge(List<Worker> listWorkers, int minAverAge) {
         int sumAge = 0;
-        for (int i = 0; i < listWorkers.size(); i++) {
-            sumAge += listWorkers.get(i).getAge();
+        for (Worker wrk : listWorkers) {
+            sumAge += wrk.getAge();
         }
-        if (sumAge / (listWorkers.size()) > minAverAge) {
-            return true;
-        }
-        return false;
+        return (sumAge / (listWorkers.size()) > minAverAge);
     }
 
-    //метод, возвращающий список сотрудников, возраст которых больше либо равен указанному аргументу;
-    private static void minAgeWorkers(ArrayList<Worker> listWorkers, int minAge) {
-        ArrayList<String> workers = new ArrayList<>();
-        for (int i = 0; i < listWorkers.size(); i++) {
-            if (listWorkers.get(i).getAge() >= minAge) {
-                workers.add(listWorkers.get(i).toString());
+    private static List<Worker> getMinAgeWorkers(List<Worker> listWorkers, int minAge) {
+        List<Worker> filteredWorkers = new ArrayList<>();
+        for (Worker wrk : listWorkers) {
+            if (wrk.getAge() >= minAge) {
+                filteredWorkers.add(wrk);
             }
         }
-        System.out.println("Список сотрудников старше " + minAge + " лет: " + workers);
+        return filteredWorkers;
     }
 
-    //метод, принимающий в качестве аргумента список сотрудников, и возвращающий список их имен
-    private static void listNameWorkers(ArrayList<Worker> listWorkers) {
-        ArrayList<String> nameWorkers = new ArrayList<>();
-        for (int i = 0; i < listWorkers.size(); i++) {
-            nameWorkers.add(listWorkers.get(i).getName());
+    private static List<String> getNamesWorkers(List<Worker> listWorkers) {
+        List<String> nameWorkers = new ArrayList<>();
+        for (Worker wrk : listWorkers) {
+            nameWorkers.add(wrk.getName());
         }
-        System.out.println("Список имен сотрудников: " + nameWorkers);
+        return nameWorkers;
     }
-
 
     //метод, увеличивающий каждый элемент списка на указанное число
-    private static void increaseList(int num, ArrayList<Integer> list1) {
-
+    private static List<Integer> increaseList(int num, List<Integer> list1) {
         for (int i = 0; i < list1.size(); i++) {
             list1.set(i, list1.get(i) + num);
         }
-        System.out.println("list1 = " + list1);
+        return list1;
     }
 
     //метод, принимающий в качестве аргументов целое число и ссылку на список, метод должен
     //переписать каждую заполненную ячейку списка указанным числом
-    private static void rewriteList(int num, ArrayList<Integer> list1) {
-
+    private static List<Integer> rewriteList(int num, List<Integer> list1) {
         for (int i = 0; i < list1.size(); i++) {
             if (list1.get(i) != 0) {
                 list1.set(i, num);
             }
         }
-        System.out.println("list1 = " + list1);
+        return list1;
     }
 
-    //    метод, суммирующий все элементы, значение которых больше 5, и возвращающий сумму
-    private static int sumElements(ArrayList<Integer> list1) {
+    private static int sumElements(List<Integer> list1) {
         int sum = 0;
         for (Integer elem : list1) {
             if (elem >= 5) {
@@ -103,14 +94,12 @@ public class mainApp {
         return sum;
     }
 
-    // метод, принимающий в качестве аргументов числа min и max, и возвращающий ArrayList с набором
-//последовательных значений в указанном диапазоне (min и max включительно, шаг - 1);
-    private static void arrayFromTo(int min, int max) {
-        ArrayList<Integer> list1 = new ArrayList<>();
+    private static List<Integer> getListFromTo(int min, int max) {
+        List<Integer> list1 = new ArrayList<>();
         for (int i = min; i <= max; i++) {
             list1.add(i);
         }
-        System.out.println(list1);
+        return list1;
     }
 
 }
